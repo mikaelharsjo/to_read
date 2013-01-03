0b6ffe68f69a7780130eeaabbf8150d49064336e
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-03 20:58:31 +0100

    launch safari on start

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     application.applicationIconBadgeNumber = 30
-    puts "#{App.name} (#{App.documents_path})"
+    url = NSURL.URLWithString("http://www.google.com/reader")
+	UIApplication.sharedApplication.openURL(url)
+
+	#if (![[UIApplication.sharedApplication] openURL:url])
+	#NSLog(@"%@%@",@"Failed to open url:",[url description]);
     true
   end
 end
