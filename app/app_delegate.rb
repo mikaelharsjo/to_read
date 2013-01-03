56dcc06a5826dd6e9caa667238dd17b31c45e3b1
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-03 20:24:36 +0100

    added bubble-wrap

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,6 +1,7 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     application.applicationIconBadgeNumber = 30
+    puts "#{App.name} (#{App.documents_path})"
     true
   end
 end
