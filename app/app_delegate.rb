1511f440df206fd55e8f5c75bc891befc99390e2
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-05 22:55:59 +0100

    use auth not sid

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,18 +1,37 @@
 class AppDelegate
+	include BubbleWrap
 	def application(application, didFinishLaunchingWithOptions:launchOptions)
-		BubbleWrap::HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU") do |response|
+		
+		HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU&accountType=GOOGLE") do |response|
 			body = response.body.to_str
+			#puts body
 			index_of_sid = body.index("SID=") + 4
-            index_of_lsid = body.index("LSID=") - 5
-            sid = resp[index_of_sid, index_of_lsid]
-			puts sid
-		end
+			index_of_lsid = body.index("LSID=") - 5
+			index_of_auth = body.index("Auth=") + 5
+			sid = body[index_of_sid, index_of_lsid]
+			auth = body[index_of_auth, body.length]
+			puts auth
+			
+			cookie = {
+				Name: 'SID',
+				Value: sid,
+				Path: '/',
+				Domain: '.google.com'
+			}
 
-		#BubbleWrap::HTTP.get("http://www.google.com/reader/api/0/unread-count?output=json") do |response|
-		#	puts response
-			#unread_counts = BubbleWrap::JSON.parse(response.body.to_str)["unreadcounts"]
-			#puts response.unreadcounts
-		#end
+			HTTP.get("http://www.google.com/reader/api/0/user-info",
+				{
+					:headers => { "Authorization:GoogleLogin auth" => auth}
+				}) do |response|
+				puts response
+				puts response.body.to_str
+			end
+
+			#puts cookie
+			#HTTP.get("http://www.google.com/reader/api/0/token", {cookie: cookie}) do |response|
+				#puts response.body.to_str
+			#end	
+		end
 		
 		application.applicationIconBadgeNumber = 30
 		
@@ -24,4 +43,17 @@ class AppDelegate
 	#NSLog(@"%@%@",@"Failed to open url:",[url description]);
 		true
  	end
+end
+
+class GoogleAuthenticator
+	def fetch_sid		
+		BubbleWrap::HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU") do |response|
+			body = response.body.to_str
+			index_of_sid = body.index("SID=") + 4
+			index_of_lsid = body.index("LSID=") - 5
+			sid = body[index_of_sid, index_of_lsid]
+			puts 'ss'
+			#puts "sid #{@sid}"
+		end
+	end
 end
