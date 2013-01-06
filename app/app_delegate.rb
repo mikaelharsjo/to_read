fad38e1c79bba7c07ae8b831f1c05afbbbdc6f64
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-06 22:38:09 +0100

    fetch unreads from google

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -10,7 +10,7 @@ class AppDelegate
 			index_of_auth = body.index("Auth=") + 5
 			sid = body[index_of_sid, index_of_lsid]
 			auth = body[index_of_auth, body.length]
-			puts auth
+			#puts auth
 			
 			cookie = {
 				Name: 'SID',
@@ -19,18 +19,18 @@ class AppDelegate
 				Domain: '.google.com'
 			}
 
-			HTTP.get("http://www.google.com/reader/api/0/user-info",
-				{
-					:headers => { "Authorization:GoogleLogin auth" => auth}
-				}) do |response|
-				puts response
-				puts response.body.to_str
-			end
+			authorization_header = "GoogleLogin auth=#{auth}" 
+			#puts authorization_header
 
-			#puts cookie
-			#HTTP.get("http://www.google.com/reader/api/0/token", {cookie: cookie}) do |response|
+			HTTP.get("http://www.google.com/reader/api/0/unread-count?output=json",
+				{
+					:headers => {"Authorization" => "GoogleLogin auth=DQAAAOEAAACiztP-p4hWZTohXl-e4LlRlRcCrfrRNV2wW3vGyaPbHKeVwIS3w9RJ0tH3MAdX7RdnXWei2gGthtzNzaAIyyuVhqYdecu_7P3fpIJVW_YCRlcvbzkOcvOS6y3Tx2OvhshLT7oLAiC7x9wusyeQPUPnSwaF8KcbG8fdrjeJ52cGINxbEL47ciwXMfApLyzqHEqdlxO7vihSoU132FXaiIeRE4sjSOEdAPLoENm1nTQ0nKCZgEXBz0m6gx79NldMhmSk3iJD6D5ENwfCF3M7kEU1WILnx2FVGsvKHNMjVShh_2HL0GlWcc18GqCjNK5u7FE"}
+				}) do |response|
+				#puts response
+				unreadcounts = JSON.parse(response.body.to_str) #.unreadcounts
+				puts unreadcounts['unreadcounts']
 				#puts response.body.to_str
-			#end	
+			end	
 		end
 		
 		application.applicationIconBadgeNumber = 30
