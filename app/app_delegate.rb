825e7f916a794f0adf8910930ce7526fede97108
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-12 23:46:34 +0100

    now gets app badge from reading-list

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -4,13 +4,11 @@ class AppDelegate
 		
 		HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU&accountType=GOOGLE") do |response|
 			body = response.body.to_str
-			#puts body
 			index_of_sid = body.index("SID=") + 4
 			index_of_lsid = body.index("LSID=") - 5
 			index_of_auth = body.index("Auth=") + 5
 			sid = body[index_of_sid, index_of_lsid]
 			auth = body[index_of_auth, body.length]
-			#puts auth
 			
 			cookie = {
 				Name: 'SID',
@@ -26,24 +24,20 @@ class AppDelegate
 				{
 					:headers => {"Authorization" => "GoogleLogin auth=DQAAAOEAAACiztP-p4hWZTohXl-e4LlRlRcCrfrRNV2wW3vGyaPbHKeVwIS3w9RJ0tH3MAdX7RdnXWei2gGthtzNzaAIyyuVhqYdecu_7P3fpIJVW_YCRlcvbzkOcvOS6y3Tx2OvhshLT7oLAiC7x9wusyeQPUPnSwaF8KcbG8fdrjeJ52cGINxbEL47ciwXMfApLyzqHEqdlxO7vihSoU132FXaiIeRE4sjSOEdAPLoENm1nTQ0nKCZgEXBz0m6gx79NldMhmSk3iJD6D5ENwfCF3M7kEU1WILnx2FVGsvKHNMjVShh_2HL0GlWcc18GqCjNK5u7FE"}
 				}) do |response|
-				#puts response
-				unreadcounts = JSON.parse(response.body.to_str)['unreadcounts'] #.unreadcounts
-				unreadcounts.each do |feed| 
-					puts(feed.count)
+				
+				json = JSON.parse(response.body.to_str)
+				json['unreadcounts'].each do |feed|
+					if feed['id'].include? 'com.google/reading-list'
+						application.applicationIconBadgeNumber = feed['count']
+					end
 				end
-
-				#puts response.body.to_str
 			end	
-		end
-		
-		application.applicationIconBadgeNumber = 30
+		end		
 		
 		# use bubble-wrap instead?
 		url = NSURL.URLWithString("http://www.google.com/reader")
 		UIApplication.sharedApplication.openURL(url)
 
-	#if (![[UIApplication.sharedApplication] openURL:url])
-	#NSLog(@"%@%@",@"Failed to open url:",[url description]);
 		true
  	end
 end
