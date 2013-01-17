390cb9ed0e710eac67bbd99eb4b14cc30e4760f1
Mikael Härsjö <mikaelharsjo@gmail.com>
2013-01-17 22:54:03 +0100

    request reader access via oauth2

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,19 +1,28 @@
 class AppDelegate
 	include BubbleWrap
+		# https://code.google.com/apis/console/#project:232700103718:access
+		CLIENT_ID = '232700103718.apps.googleusercontent.com'
+		CLIENT_SECRET = 'msGejkhE_enVitNpjJOYyneN'	
+
 	def application(application, didFinishLaunchingWithOptions:launchOptions)
-		signInAndGetUnreadCount do |count| 
-			puts(count)
-		end
+		redirect_uri = 'http://localhost'
+		scope = 'http://www.google.com/reader/api/*'
+		response_type = 'code'
+		App.open_url("https://accounts.google.com/o/oauth2/auth?response_type=#{response_type}&client_id=#{CLIENT_ID}&redirect_uri=#{redirect_uri}&scope=#{scope}")
+
+		#signInAndGetUnreadCount do |count| 
+		#	puts(count)
+		#end
 		
 		# use bubble-wrap instead?
-		url = NSURL.URLWithString("http://www.google.com/reader")
-		UIApplication.sharedApplication.openURL(url)		
+		#url = NSURL.URLWithString("http://www.google.com/reader")
+		#UIApplication.sharedApplication.openURL(url)		
 
 		true
  	end
 
  	def signInAndGetUnreadCount(&block)
- 		HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU&accountType=GOOGLE") do |response|
+ 		HTTP.get("https://www.google.com/accounts/ClientLogin?service=reader&Email=mikaelharsjo@gmail.com&Passwd=CA5WAC2WpG4LarHuItrmBzzrMZddxdelvIZ81IFhjbnhhpmiJU&accountType=GOOGLE&continue=http://www.google.com&source=to_read") do |response|
  			@authCookie = parseCookie(response)
  			getUnreadCount(&block)
  		end
@@ -32,7 +41,9 @@ class AppDelegate
 
  	def getUnreadCount(&block)
  		# TODO: fix later
- 		@authCookie = 'DQAAAOEAAACiztP-p4hWZTohXl-e4LlRlRcCrfrRNV2wW3vGyaPbHKeVwIS3w9RJ0tH3MAdX7RdnXWei2gGthtzNzaAIyyuVhqYdecu_7P3fpIJVW_YCRlcvbzkOcvOS6y3Tx2OvhshLT7oLAiC7x9wusyeQPUPnSwaF8KcbG8fdrjeJ52cGINxbEL47ciwXMfApLyzqHEqdlxO7vihSoU132FXaiIeRE4sjSOEdAPLoENm1nTQ0nKCZgEXBz0m6gx79NldMhmSk3iJD6D5ENwfCF3M7kEU1WILnx2FVGsvKHNMjVShh_2HL0GlWcc18GqCjNK5u7FE'
+ 		puts "authCookie: #{@authCookie}"
+ 		#@authCookie = 'DQAAAOEAAACiztP-p4hWZTohXl-e4LlRlRcCrfrRNV2wW3vGyaPbHKeVwIS3w9RJ0tH3MAdX7RdnXWei2gGthtzNzaAIyyuVhqYdecu_7P3fpIJVW_YCRlcvbzkOcvOS6y3Tx2OvhshLT7oLAiC7x9wusyeQPUPnSwaF8KcbG8fdrjeJ52cGINxbEL47ciwXMfApLyzqHEqdlxO7vihSoU132FXaiIeRE4sjSOEdAPLoENm1nTQ0nKCZgEXBz0m6gx79NldMhmSk3iJD6D5ENwfCF3M7kEU1WILnx2FVGsvKHNMjVShh_2HL0GlWcc18GqCjNK5u7FE'
+		puts "authCookie: #{@authCookie}"
 		HTTP.get("http://www.google.com/reader/api/0/unread-count?output=json",
 		{
 			:headers => {"Authorization" => "GoogleLogin auth=#{@authCookie}"}
